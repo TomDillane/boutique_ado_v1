47860cb6cf5537553dd4a97e12beac5a161df190
TomDillane <tommy.c.dillane@gmail.com>
2021-06-30 12:21:12 +0000

    hostname update

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -9,6 +9,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 import dj_database_url
+if os.path.exists("env.py"):
+    import env
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -23,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['td-django-todo-app.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['td-boutique-ado.herokuapp.com', 'localhost']
 
 
 # Application definition
